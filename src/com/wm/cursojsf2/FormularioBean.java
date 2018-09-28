1e478868894054dd8182465676e54618c7c41a76
willi <willi@LAPTOP-B6ER7NR3>
2018-09-28 07:21:56 -0300

    3.5

---
+++ src/com/wm/cursojsf2/FormularioBean.java
@@ -0,0 +1,18 @@
+package com.wm.cursojsf2;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class FormularioBean {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
