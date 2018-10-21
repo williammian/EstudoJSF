2f8aefd874bcb40656b0e082bb83554cd526261c
willi <willi@192.168.0.2>
2018-10-21 18:58:56 -0200

    6.3

---
+++ src/com/wm/cursojsf2/TesteAjaxBean.java
@@ -0,0 +1,18 @@
+package com.wm.cursojsf2;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TesteAjaxBean {
+
+	private String descricao;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
